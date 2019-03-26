20ee37170d32377aa2f2fc09788e18cff58f0f99
İsa <isatoltar@gmail.com>
2019-03-26 12:48:44 +0300

    routes added

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var wordRoutes = require('./routes/wordRoute');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/words',wordRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/wordRoute.js
+++ server/routes/wordRoute.js
@@ -2,9 +2,70 @@ const express = require('express');
 const router = express.Router;
 const Word = require('../models/Word');
 
-router.get('/', (req,res)=> {
-    res.send();
+// add a word
+router.post('/', (req,res)=> {
+    let newWord = new Word(req.body);
+
+    newWord.save()
+            .then(data => {
+                res.json(data);
+            })
+            .catch(err=> {
+                res.json(err);
+            });
+
 });
+//get all words
+router.get('/', (req,res)=> {
+    newWord.find({})
+            .then(data=> {
+                res.json(data);
+            })
+            .catch(err=> {
+                res.json(err);
+            })
+});
+
+//get a word by id
+router.get('/:id',(req,res) =>{
+    let query = {_id : req.params.id};
+    Word.findById(query)
+        .then(data =>{
+            res.status(200).json(data);
+        })
+        .catch(err=> {
+            res.status(404).json(err);
+        })
+});
+
+//update a word
+router.put('/update/:id', (req,res)=> {
+    let query = {_id: req.params.id};
+    Word.findByIdAndUpdate(query, req.body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+
+//delete a word
+router.delete('/delete/:id', (req,res)=> {
+    let query = {_id : req.params.id};
+    Word.findByIdAndRemove(query)
+        .then((data)=>{
+            res.json(data);
+        })
+        .catch(err =>{
+            res.json(err);
+        })
+})
+
+
+
+
 
 
 module.exports = router;
