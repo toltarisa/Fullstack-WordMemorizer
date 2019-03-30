66e502f0bbe0163247311054f2cbb0ae93e95ea6
İsa <isatoltar@gmail.com>
2019-03-30 22:55:15 +0300

    some minor changes

--- client/src/components/common/Form.js
+++ client/src/components/common/Form.js
@@ -4,7 +4,6 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default class FormDialog extends React.Component {  
@@ -15,12 +14,8 @@ export default class FormDialog extends React.Component {
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogTitle id="form-dialog-title">Kelime Bilgilerini Giriniz</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              To subscribe to this website, please enter your email address here. We will send
-              updates occasionally.
-            </DialogContentText>
             <TextField
               autoFocus
               margin="normal"
@@ -41,7 +36,7 @@ export default class FormDialog extends React.Component {
               autoFocus
               margin="normal"
               id="tur"
-              label="Kelimenin Turunu Girin"
+              label="Kelimenin Türünü Girin"
               type="text"
               fullWidth
             />
